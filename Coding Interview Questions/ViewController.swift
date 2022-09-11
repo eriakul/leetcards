e84fc09fdbd5a71e1638ee3a714c0ad9aa5f8d88
eriakul <lu.erika58@gmail.com>
2022-09-10 18:09:18 -0700

    Initial Commit

---
+++ Coding Interview Questions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coding Interview Questions
+//
+//  Created by Erika Lu on 9/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
